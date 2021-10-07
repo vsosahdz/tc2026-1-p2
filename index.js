229a5a47f5a1ca7a53d5f2e8d54e9b7e1e9930b4
Víctor Adrián Sosa Hernández <vsosa@tec.mx>
2021-10-06 19:19:27 -0500

    Servidor de prueba

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express")
+const app = express()
+
+app.get("/prueba",(req,res)=>{
+    res.send("Prueba del servidor funcionando")
+})
+
+app.listen(8084,()=>{
+    console.log("Servidor en línea por el puerto 8084")
+})
